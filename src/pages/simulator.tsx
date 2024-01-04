3859434e0290cab7d57377b54c4880f2a326b159
tracymedcalf <20527084+tracymedcalf@users.noreply.github.com>
2024-01-04 08:42:06 -0800

    Grid bg extends to bottom

--- src/pages/simulator.tsx
+++ src/pages/simulator.tsx
@@ -1,60 +1,36 @@
-import { useRef, useEffect, useState } from "react";
+import GridLayout from "react-grid-layout";
 
-class Entity {
-    element: HTMLDivElement;
-    constructor() {
-        this.element = document.createElement("div");
-    }
-}
-
-class Racking extends Entity {
-
-    constructor() {
-        super();
-        this.element.setAttribute(
-            "style",
-            "background-color: orange; width: 20px; height: 20px;"
-        );
-    }
-}
-
-class Hotspot extends Entity {
-
-    constructor() {
-        super();
-        this.element.setAttribute(
-            "style",
-            "background-color: orange;"
-        );
-    }
-}
 
 export default function Simulator() {
-
-    const divRef = useRef<HTMLDivElement>(null);
-
-    const [state, setState] = useState<Entity[]>([]);
-
-    const buttonHandler = () => {
-        setState([...state, new Racking()]);
+    const cols = 13;
+    const width= 1300;
+    const cellSize = width / cols;
+    const style = {
+        "height": "100vh",
+        "backgroundImage": "repeating-linear-gradient(#ccc 0 1px, transparent 1px 100%),repeating-linear-gradient(90deg, #ccc 0 1px, transparent 1px 100%)",
+        "backgroundSize": `${cellSize}px ${cellSize}px`
     };
-
-    useEffect(() => {
-        const div = divRef.current;
-        if (div == null) throw "DIV ref is null";
-    }, [state]);
+    const layout = [
+        { i: "a", x: 0, y: 0, w: 1, h: 1 },
+        { i: "b", x: 1, y: 0, w: 1, h: 1 },
+        { i: "c", x: 4, y: 4, w: 1, h: 1 }
+    ];
 
     return (
-        <div>
-            <div ref={divRef}>
-
-            </div>
-            <button onClick={buttonHandler}>Create Entity</button>
+        <div className="h-screen">
+            <GridLayout
+                className="layout h-screen w-full"
+                margin={[0, 0]}
+                layout={layout}
+                cols={cols}
+                rowHeight={cellSize}
+                width={width}
+                style={style}
+            >
+                <div className="bg-orange-500 m-0" key="a">a</div>
+                <div className="bg-orange-500 m-0" key="b">b</div>
+                <div className="bg-orange-500 m-0" key="c">c</div>
+            </GridLayout>
         </div>
     );
-}
-
-
-function effect(div: HTMLDivElement) {
-
 }
