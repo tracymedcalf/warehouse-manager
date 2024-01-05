e8ba4ae29d7db1e3b1adcce5360d86f5c85c4307
tracymedcalf <20527084+tracymedcalf@users.noreply.github.com>
2024-01-04 17:38:37 -0800

    Racking appears at btn click

--- src/pages/simulator.tsx
+++ src/pages/simulator.tsx
@@ -24,9 +24,7 @@ type Entity = Hotspot | Racking | Mod
 
 export default function Simulator() {
 
-    const [entities, setEntities] = useState<Entity[]>([
-        { id: uuidv4(), x: 1, y: 1, type: "racking" },
-    ]);
+    const [entities, setEntities] = useState<Entity[]>([]);
 
     const [selected, setSelected] = useState<Entity | null>(null);
 
@@ -39,35 +37,44 @@ export default function Simulator() {
         }); 
     };
 
-    const handleClick = (ev: React.MouseEvent) => {
+    const handleClick = (row: number, col: number) => {
 
         if (selected == null) return;
-
+        setEntities([...entities, {...selected, x: col, y: row }]);
         setSelected(null);
     };
 
+    const Row = ({ row }: { row: number }) => {
+        const a = [];
+        for (let i = 0; i < 12; i++) {
+            a.push(
+                <div
+                    className="border inline-block m-0 w-10 h-10 border-white-900"
+                    onClick={() => handleClick(row, i)}
+                >
+                </div>
+            );
+        }
+        return a;
+    };
+
+    const Grid = () => {
+        const a = [];
+        for (let i = 0; i < 10; i++) {
+            a.push(
+                <div className="-mt-8px m-0 p-0">
+                    <Row row={i} />
+                </div>
+            );
+        }
+        return a;
+    };
+
     return (
-        <div>
-            <div
-                onClick={handleClick}
-                className="bg-gray-400 w-screen h-screen"
-            >
-
-                {entities.map(({ id, x, y }) => {
-                    const style = { top: x + "px", left: y + "px" };
-                    return (
-                        <div
-                            key={id}
-                            className="bg-orange-500 w-10 h-10"
-                            style={style}
-                        >
-
-                        </div>
-                    );
-                })}
-
-            </div>
-            <button onClick={handleNewRacking}>New Racking</button>
+        <div style={{ paddingTop: "8px"}}>
+            {entities?.map(e => <div className="absolute w-10 h-10 z-10 bg-orange-500"></div>)}
+            <Grid />
+            <button onClick={handleNewRacking}>Add Racking</button>
         </div>
     );
 }
